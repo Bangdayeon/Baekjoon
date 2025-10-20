86f7e36974a324f3d9d14295ab96eaf431a09401
Bangdayeon <dybang00@gmail.com>
2025-10-21 00:28:32 +0900

    create.js 수정

--- create.js
+++ create.js
@@ -28,10 +28,8 @@ if (fs.existsSync(jsFilePath)) {
 // 3. 입력 형식에 따른 템플릿
 const templates = {
   1: `
-const fs = require("fs") ;
-const path = require("path");
-const filePath = process.platform === "linux" ? "/dev/stdin" : path.join(__dirname, "run", "input.txt");
-const input = Number(fs.readFileSync(filePath).toString().trim());
+const filePath = process.platform === "linux" ? "/dev/stdin" : require("path").join(__dirname, "run", "input.txt");
+const input = Number(require("fs").readFileSync(filePath).toString().trim());
 
 function solution(n) {
   console.log(n);
@@ -39,10 +37,8 @@ function solution(n) {
 
 solution(input);`,
   2: `
-const fs = require("fs");
-const path = require("path");
-const filePath = process.platform === "linux" ? "/dev/stdin" : path.join(__dirname, "run", "input.txt");
-const input = fs.readFileSync(filePath).toString().trim().split(" ").map(Number);
+const filePath = process.platform === "linux" ? "/dev/stdin" : require("path").join(__dirname, "run", "input.txt");
+const input = require("fs").readFileSync(filePath).toString().trim().split(" ").map(Number);
 
 function solution(arr) {
   console.log(arr);
@@ -50,10 +46,8 @@ function solution(arr) {
 
 solution(input);`,
   3: `
-const fs = require("fs");
-const path = require("path");
-const filePath = process.platform === "linux" ? "/dev/stdin" : path.join(__dirname, "run", "input.txt");
-const input = fs.readFileSync(filePath).toString().trim().split("\\n").map(Number);
+const filePath = process.platform === "linux" ? "/dev/stdin" : require("path").join(__dirname, "run", "input.txt");
+const input = require("fs").readFileSync(filePath).toString().trim().split("\\n").map(Number);
 const [n, ...arr] = input;
 
 function solution(n, arr) {
@@ -62,10 +56,8 @@ function solution(n, arr) {
 
 solution(n, arr);`,
   4: `
-const fs = require("fs");
-const path = require("path");
-const filePath = process.platform === "linux" ? "/dev/stdin" : path.join(__dirname, "run", "input.txt");
-const input = fs.readFileSync(filePath).toString().trim().split(/\\s+/);
+const filePath = process.platform === "linux" ? "/dev/stdin" : require("path").join(__dirname, "run", "input.txt");
+const input = require("fs").readFileSync(filePath).toString().trim().split(/\\s+/);
 let [n, ...arr] = input.map(Number);
 
 function solution(n, arr) {
