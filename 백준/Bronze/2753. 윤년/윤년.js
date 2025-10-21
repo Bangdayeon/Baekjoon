6cafd69a0f94b82fff2465ba0ba59d87bc5aa723
Bang Dayeon <134805973+Bangdayeon@users.noreply.github.com>
2025-10-21 19:17:28 +0900

    [Bronze V] Title: 윤년, Time: 100 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.js
@@ -0,0 +1,5 @@
+const input = Number(require("fs").readFileSync("/dev/stdin").toString().trim());
+
+const year = input;
+if(year%4 === 0 && (year%100 !== 0 || year%400===0)) console.log(1);
+else console.log(0);
