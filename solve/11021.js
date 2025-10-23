ec9990d8a43a70a7482378060e756858c4fa1115
Bangdayeon <dybang00@gmail.com>
2025-10-23 16:39:27 +0900

    11021. Case #n: a+b-3m

---
+++ solve/11021.js
@@ -0,0 +1,14 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : require("path").join(__dirname, "run", "input.txt");
+const input = require("fs").readFileSync(filePath).toString().trim().split(/\s+/);
+let [n, ...arr] = input.map(Number);
+
+function solution(n, arr) {
+  let result = [];
+  let j=1;
+  for(let i=0;i<n*2;i+=2) {
+    result.push(`Case #${j++}: ${arr[i]+arr[i+1]}`);
+  }
+  console.log(result.join('\n'));
+}
+
+solution(n, arr);
