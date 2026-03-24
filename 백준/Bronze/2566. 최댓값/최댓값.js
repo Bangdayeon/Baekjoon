3a7baee2b97f514679d13f0157c1f7e371b3408f
Bang Dayeon <134805973+Bangdayeon@users.noreply.github.com>
2026-03-24 23:23:49 +0900

    [Bronze III] Title: 최댓값, Time: 92 ms, Memory: 9352 KB -BaekjoonHub

---
+++ 백준/Bronze/2566. 최댓값/최댓값.js
@@ -0,0 +1,20 @@
+const filePath = process.platform === "linux" ? "/dev/stdin" : require("path").join(__dirname, "run", "input.txt");
+const input = require("fs").readFileSync(filePath).toString().trim().split(/\s+/);
+let [...arr] = input.map(Number);
+
+function solution(arr) {
+  let max = 0;
+  let [n, m] = [0,0];
+
+  for(let i=0; i<81; i++){
+    if(arr[i] > max) {
+      max = arr[i];
+      n = Math.trunc(i/9);
+      m = i % 9;
+    };
+  }
+  console.log(max);
+  console.log(n+1, m+1)
+}
+
+solution(arr);
