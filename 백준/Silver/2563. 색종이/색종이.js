88b6360383be373b4c43dfc8e3b8254e9dd479cb
Bang Dayeon <134805973+Bangdayeon@users.noreply.github.com>
2026-03-25 23:25:39 +0900

    [Silver V] Title: 색종이, Time: 92 ms, Memory: 9684 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.js
+++ 백준/Silver/2563. 색종이/색종이.js
@@ -3,17 +3,13 @@ const input = require("fs").readFileSync(filePath).toString().trim().split('\n')
 let [n, ...arr] = input;
 
 function solution(n, arr) {
-  let black = new Array(100).fill(0);
+  let black = Array.from({length: 100}, () => Array(100).fill(0));
   let answer = 0;
 
-  for(let i=0;i<100;i++) {
-    black[i] = new Array(100).fill(0);
-  }
-
   for(let i=0; i<n; i++) {
-    let white = arr[i];
-    let [startx, starty] = white.split(' ');
-    let [endx, endy] = [Number(startx)+10, Number(starty)+10];
+    let [startx, starty] = arr[i].split(' ').map(Number);
+    let [endx, endy] = [startx+10, starty+10];
+    
     for(let j=startx; j<endx; j++) {
       for(let k=starty; k<endy; k++) {
         black[j][k] = 1;
