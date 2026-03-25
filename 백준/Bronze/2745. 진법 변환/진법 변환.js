a16e72849ea2f1cd08311f7b543609a26b044032
Bang Dayeon <134805973+Bangdayeon@users.noreply.github.com>
2026-03-25 23:58:50 +0900

    [Bronze II] Title: 진법 변환, Time: 148 ms, Memory: 9648 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.js
+++ 백준/Bronze/2745. 진법 변환/진법 변환.js
@@ -2,23 +2,7 @@ const filePath = process.platform === "linux" ? "/dev/stdin" : require("path").j
 const input = require("fs").readFileSync(filePath).toString().trim().split(" ");
 
 function solution(arr) {
-  let n = arr[0];
-  let b = Number(arr[1]);
-  let answer = 0;
-
-  for(let i=0;i<n.length; i++) {
-    let char = n[i];
-    let value;
-
-    if(char >= 'A') {
-      value = char.charCodeAt(0) - 55;
-    } else {
-      value = Number(char);
-    }
-
-    answer = answer * b + value;
-  }
-  console.log(answer);
+  console.log(parseInt(arr[0], Number(arr[1])));
 }
 
 solution(input);
